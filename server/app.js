386af5c11a69fa1aab09b302d4269dc90fb920e4
Devin Wilson <wilson@Devins-Air.attlocal.net>
2020-06-03 13:08:56 -0700

    final work,before now deployment

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // import files and packages up here
-
+const morgan = require('morgan');
+const express = require('express');
+const http = require('http');
+const data = require('./data.json')
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.status(200).send('New Express Server Up and Running!');
+})
+
+app.get('/data', function(req, res) {
+    res.json(data);
+})
 
 // finally export the express application
 module.exports = app;
